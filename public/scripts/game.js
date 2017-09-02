94400a24c20a31f90d82deb42afdf0de0b9e4ec4
Aislin Jackson <aislin.ms.jackson@gmail.com>
2017-09-02 18:35:07 +0000

    Add logic for drawing and displaying a hand

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -2,7 +2,38 @@ var HANDSIZE        = 7;
 var SHOWSIZE        = 3;
 var discard         = [];
 var hand            = [];
-var displayCards    = document.querySelectorAll(".card");
+var displayCards    = document.querySelectorAll(".card img");
 var draw            = JSON.parse(document.querySelector(".invisible").textContent);
 var drawButton      = document.getElementById("drawButton");
 var discardButton   = document.getElementById("discardButton");
+
+init();
+
+function init() {
+    drawButton.addEventListener("click", function(){
+        drawCards();
+        refresh();
+    });
+}
+
+function drawCards() {
+    var handsize = HANDSIZE;
+    if (draw.length < HANDSIZE) {
+        handsize = draw.length;
+    }
+    
+    for(var i =0; i < handsize; i++){
+        hand.push(draw.pop());
+    }
+}
+
+function refresh() {
+    for(var i=0; i < displayCards.length; i++){
+        if(hand[i]){
+            displayCards[i].style.display = "inline";
+            displayCards[i].src = hand[i].imageURL;
+        } else {
+            displayCards[i].style.display = "none";
+        }
+    }
+}
