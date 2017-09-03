ee058719f195f3b57af1934f376b06913659fff3
Aislin Jackson <aislin.ms.jackson@gmail.com>
2017-09-03 02:54:02 +0000

    Add ability to select cards

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,12 +1,13 @@
-var HANDSIZE        = 7;
-var SHOWSIZE        = 3;
-var discard         = [];
-var hand            = [];
-var selectedCards   = [];
-var displayCards    = document.querySelectorAll(".card img");
-var draw            = JSON.parse(document.querySelector(".invisible").textContent);
-var drawButton      = document.getElementById("drawButton");
-var discardButton   = document.getElementById("discardButton");
+var HANDSIZE            = 7;
+var SHOWSIZE            = 3;
+var discard             = [];
+var hand                = [];
+var selectedCards       = [];
+var numCardsSelected    = 0;
+var displayCards        = document.querySelectorAll(".card img");
+var draw                = JSON.parse(document.querySelector(".invisible").textContent);
+var drawButton          = document.getElementById("drawButton");
+var discardButton       = document.getElementById("discardButton");
 
 init();
 
@@ -28,6 +29,19 @@ function init() {
         selectedCards = [];
         refresh(); 
     });
+    
+    // Listeners for each displayCard
+    for(var i= 0; i < displayCards.length; i++){
+        displayCards.item(i).addEventListener("click", function() {
+            if(!this.classList.contains("selected") && numCardsSelected < SHOWSIZE) {
+                this.classList.add("selected");
+                numCardsSelected++;
+            } else if (this.classList.contains("selected")) {
+                this.classList.remove("selected");
+                numCardsSelected--;
+            }
+        });
+    }
 }
 
 function drawCards() {
